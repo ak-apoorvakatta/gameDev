0a7c2c2431f92230e0d7b4166b2c97987464d151
chocolateyuki <vaishali.gir@gmail.com>
2017-03-27 15:31:03 +0800

    First Project Commit

---
+++ DeadEye Rumble/Assets/Scripts/FollowTheCursor.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowTheCursor : MonoBehaviour {
+
+	public float speed = 100.0f;
+	private Vector3 destination;
+
+	// Use this for initialization
+	void Start () {
+		destination = transform.position;
+		Cursor.visible = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Change cursor position to position of mouse pressed
+		if (Input.mousePresent) {
+			float distance = transform.position.z - Camera.main.transform.position.z;
+
+			destination = new Vector3 (Input.mousePosition.x, Input.mousePosition.y, distance);
+
+			destination = Camera.main.ScreenToWorldPoint (destination);
+			//transform.position = destination;
+		}
+
+		//transform.position = Vector3.Lerp (transform.position, destination, speed * Time.deltaTime);
+		transform.position = destination;
+
+
+	}
+}
